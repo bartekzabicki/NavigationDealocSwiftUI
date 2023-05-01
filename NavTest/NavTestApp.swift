e923ca2bec6ec02e6287f32f4cbb126088f7aa67
Bartek Żabicki <bartekzabicki@gmail.com>
2023-05-01 08:58:02 +0200

    Initial Commit

---
+++ NavTest/NavTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavTestApp.swift
+//  NavTest
+//
+//  Created by Bart on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
